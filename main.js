17e7576b0bf18a3eb62fe67e53d6a31c0258c71e
Harsh Sorathiya <113373860+Harsh-3011@users.noreply.github.com>
2023-01-13 17:07:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+	var navLinks = document.getElementById("navLinks");
+
+	function showmenu(){
+		navLinks.style.right = "0";
+	}
+	function hidemenu(){
+		navLinks.style.right = "-200px"
+	}
+	window.addEventListener('scroll', function(){
+		var scroll = document.querySelector('.scrolltoTop');
+		scroll.classList.toggle("active" , window.scrollY > 500)
+	})
+
+	function scrollToTop(){
+		window.scrollTo({
+			top:0,
+			behaviour:'smooth'
+		})
+	}
+
+	window.addEventListener('scroll' , function(){
+		var header = document.querySelector("header");
+		header.classList.toggle("sticky", window.scrollY > 0);
+	})
